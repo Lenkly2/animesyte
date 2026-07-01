e61fefea47aeeec038e686f1b035a7a442293d79
Admin <lenklygamebs@gmail.com>
2026-07-01 22:58:25 +0300

    finalweb2

--- main/views.py
+++ main/views.py
@@ -51,7 +51,7 @@ class CreateRoomView(CreateView):
 class RegisterView(FormView):
     template_name = "register.html"
     form_class = UserCreationForm
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("login")
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
